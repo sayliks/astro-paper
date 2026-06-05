32e7cf37483c20c099acf7fdedf2ed48864fcddc
sayliks <rain.view@outlook.com>
2026-06-05 10:52:11 +0800

    Remove Pinterest share option

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -50,7 +50,6 @@ export default defineAstroPaperConfig({
     { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
     { name: "x",        url: "https://x.com/intent/post?url=" },
     { name: "telegram", url: "https://t.me/share/url?url=" },
-    { name: "pinterest", url: "https://pinterest.com/pin/create/button/?url=" },
     { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
   ],
 });
